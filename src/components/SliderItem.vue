8cd23afcc78071b639d1e367bf5632bf990c22cc
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2021-03-03 21:34:41 +0700

    slider item as link to product

--- src/components/SliderItem.vue
+++ src/components/SliderItem.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="sliderItem">
+    <router-link :to="'/product/' + productInfo.id"  class="sliderItem" tag="div">
         <img :src="productInfo.image" alt="product image" class="sliderItem__image">
         <div class="sliderItem-price">
             <p v-if="productInfo.discountPrice.length" class="sliderItem-price__price">{{productInfo.discountPrice}}₽</p>
             <p :class="{'sliderItem-price__sale': productInfo.discountPrice.length}">{{productInfo.price}}₽</p>
         </div>
         <p class="sliderItem__model">{{productInfo.model}}</p>
-    </div>
+    </router-link>
 </template>
 
 <script>
@@ -61,6 +61,13 @@
                 text-decoration: line-through;
                 color: red;
             }
+        }
+
+        &:hover {
+            box-shadow: 0 0 10px rgba(0,0,0,0.5);
+            border-radius: 7px;
+            transition: .3s;
+            cursor: pointer;
         }
     }
 </style>
